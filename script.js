12d5bc693b9833f3291a3ca4eda008238c71e115
Cristobal Jesus <crisjg10@gmail.com>
2025-04-10 13:48:52 +0200

    Supabase settings

--- script.js
+++ script.js
@@ -14,8 +14,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const statusSelect = document.getElementById('status');
 
     // Configuración de Supabase (REEMPLAZA CON TUS CREDENCIALES)
-    const supabaseUrl = 'YOUR_SUPABASE_URL';
-    const supabaseAnonKey = 'YOUR_SUPABASE_ANON_KEY';
+    const supabaseUrl = 'https://kfecdtnoxudjnelipkff.supabase.co';
+    const supabaseAnonKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImtmZWNkdG5veHVkam5lbGlwa2ZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDQyODEwODQsImV4cCI6MjA1OTg1NzA4NH0.CsBIUfPTvzbbMrXuyYfq-JXAXRt_HSilbGczY6r_jUY';
     const supabase = createClient(supabaseUrl, supabaseAnonKey);
 
     async function fetchLibrary() {
