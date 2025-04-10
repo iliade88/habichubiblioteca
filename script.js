f719b361ecb635c5cea32dde998477f21e7c4d0e
Cristobal Jesus <crisjg10@gmail.com>
2025-04-10 13:52:52 +0200

    Loading supabase from CDN

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-import { createClient } from '@supabase/supabase-js';
-
 document.addEventListener('DOMContentLoaded', () => {
     const searchInput = document.getElementById('search-input');
     const searchButton = document.getElementById('search-button');
